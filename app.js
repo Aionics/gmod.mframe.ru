63d49f89ffb1df15c2b145b8fe2cc181decc07c5
aionics <aionics@list.ru>
2016-09-17 02:04:41 +0500

    api added

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 var express = require('express');
 var app = express();
-var path = require("path");
+var bodyParser = require("body-parser");
 var PORT = 8083
+
+var path = require("path");
+var Gamedig = require('gamedig');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var respond = function(err, result) {
+    if(err == undefined) {
+        err = null
+    }
+
+    if(result == undefined) {
+        result = null
+    }
+
+    this.json({
+        err: err,
+        result: result
+    })
+}
+
+app.use(function(req, res, next) {
+    res.respond = function() {
+        respond.apply(res, arguments)
+    }
+})
+
+app.post("/api/getserverdata", function(req, res) {
+    // Gamedig.query(
+    //     {
+    //         type: 'garrysmod',
+    //         host: 'mframe.ru'
+    //     },
+    //     function(state) {
+    //         if (state.error) {
+    //             res.respond("Server is offline");
+    //         } else {
+    //             var answer = {
+    //                 map: state.map,
+    //                 playersamount: state.raw.numplayers,
+    //                 maxplayers: state.maxplayers
+    //             }
+                res.respond("answer")
+    //         }
+    //     }
+    // );
+});
 
 app.use('/', express.static('./www'));
 app.get('/*', function (req, res) {
