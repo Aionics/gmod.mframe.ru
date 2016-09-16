baa18208aaf3964c0f53a6b86341af85d5071277
aionics <aionics@list.ru>
2016-09-17 03:38:48 +0500

    bootstrap added statistics update

--- www/index.js
+++ www/index.js
@@ -2,12 +2,29 @@ var site = {
 
     currentPage: ko.observable(),
     pages: ko.observableArray(),
+    currnetMap: ko.observable(),
+    maxPlayers: ko.observable(),
+    playersAmount: ko.observable(),
+    playersList: ko.observableArray(),
 
     goto: function(Direction) {
         site.currentPage(Direction);
         pager.navigate(Direction);
-    }
+    },
 
+    loadAll: function() {
+        var serverData = $.ajax({
+            url: 'api/getserverdata',
+            dataType: 'json',
+            type: 'post',
+            async: false
+        }).responseJSON;
+
+        site.currnetMap(serverData.map);
+        site.playersAmount(serverData.playersamount);
+        site.maxPlayers(serverData.maxplayers);
+        site.playersList(serverData.palyerslist);
+    }
 }
 
 
@@ -25,5 +42,7 @@ $( document ).ready(function() {
         site.currentPage(pager.page.route[0])
     })
     pager.navigate("statistics");
+
+    setTimeout(site.loadAll, 200);
 
 });
