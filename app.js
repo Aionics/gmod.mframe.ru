a9ff54fa1e65a4551f41db9819180f5e1a7a5203
aionics <aionics@list.ru>
2016-09-17 02:16:56 +0500

    api fix

--- app.js
+++ app.js
@@ -6,49 +6,50 @@ var PORT = 8083
 var path = require("path");
 var Gamedig = require('gamedig');
 
+app.set('json spaces', 40);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var respond = function(err, result) {
-    if(err == undefined) {
-        err = null
-    }
-
-    if(result == undefined) {
-        result = null
-    }
-
-    this.json({
-        err: err,
-        result: result
-    })
-}
-
-app.use(function(req, res, next) {
-    res.respond = function() {
-        respond.apply(res, arguments)
-    }
-})
+// var respond = function(err, result) {
+//     if(err == undefined) {
+//         err = null
+//     }
+//
+//     if(result == undefined) {
+//         result = null
+//     }
+//
+//     this.json({
+//         err: err,
+//         result: result
+//     })
+// }
+//
+// app.use(function(req, res, next) {
+//     res.respond = function() {
+//         respond.apply(res, arguments)
+//     }
+// })
 
 app.post("/api/getserverdata", function(req, res) {
-    // Gamedig.query(
-    //     {
-    //         type: 'garrysmod',
-    //         host: 'mframe.ru'
-    //     },
-    //     function(state) {
-    //         if (state.error) {
-    //             res.respond("Server is offline");
-    //         } else {
-    //             var answer = {
-    //                 map: state.map,
-    //                 playersamount: state.raw.numplayers,
-    //                 maxplayers: state.maxplayers
-    //             }
-                res.respond("answer")
-    //         }
-    //     }
-    // );
+    Gamedig.query(
+        {
+            type: 'garrysmod',
+            host: 'mframe.ru'
+        },
+        function(state) {
+            if (state.error) {
+                res.end("Server is offline");
+            } else {
+                var answer = {
+                    map: state.map,
+                    playersamount: state.raw.numplayers,
+                    maxplayers: state.maxplayers
+                }
+                res.json(answer)
+            }
+        }
+    );
 });
 
 app.use('/', express.static('./www'));
