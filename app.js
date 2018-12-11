cffcd1f8094f0a6a0931732cec3f481f59f8cb9f
Artem Kosarev <aionics@list.ru>
2018-12-11 11:35:04 +0300

    prerender

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 const express = require('express');
+const nunjucks = require('nunjucks')
 const app = express();
-const server  = require('http').createServer(app);
+const server = require('http').createServer(app);
 const bodyParser = require("body-parser");
 const path = require("path");
 
-app.set('json spaces', 40);
+nunjucks.configure('./www/views', {
+    autoescape: true,
+    express: app
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api', require('./api'));
 const chat = require('./chat').socket(app, server)
-
+app.use('/api', require('./api'));
 app.use('/', express.static('./www'));
-app.get('/*', function (req, res, next) {
-    if(req.url.indexOf('/api') !== -1) return next();
-    res.sendFile("index.html", { root: __dirname + "/www"} )
+let pages = [
+    {
+        id: "main",
+        name: "Статистика",
+        file: "index.html",
+        url: "/"
+    },
+    {
+        id: "rules",
+        name: "Правила",
+        file: "rules.html",
+        url: "/rules"
+    }
+]
+
+Object.keys(pages).forEach((page_index) => {
+    let page = pages[page_index]
+    console.log(page);
+    app.get(page.url, (req, res) => {
+        return res.render(page.file, {pages: pages, current_page: page})
+    })
+})
+app.use((req, res, next) => {
+    return res.status(404).send('Sorry cant find that!');
 });
 
 const PORT = 8083
--- www/index.js
+++ www/index.js
@@ -1,17 +1,5 @@
 var site = {
 
-    currentPage: ko.observable(),
-    pages: ko.observableArray(),
-    currnetMap: ko.observable(),
-    maxPlayers: ko.observable(),
-    playersAmount: ko.observable(),
-    playersList: ko.observableArray(),
-
-    goto: function(Direction) {
-        site.currentPage(Direction);
-        pager.navigate(Direction);
-    },
-
     loadAll: function() {
         var serverData = $.ajax({
             url: 'api/getserverdata',
@@ -20,20 +8,16 @@ var site = {
             async: false
         }).responseJSON;
 
-        serverData.palyerslist.forEach(function (player, index, players) {
-            var hours = Math.floor(player.time / 60 / 60)
-            var minutes = Math.floor((player.time - hours * 60 * 60) / 60)
-            var time = {
-                hours: hours,
-                minutes: minutes
-            }
-            players[index].time = time;
-        })
+        // serverData.palyerslist.forEach(function (player, index, players) {
+        //     var hours = Math.floor(player.time / 60 / 60)
+        //     var minutes = Math.floor((player.time - hours * 60 * 60) / 60)
+        //     var time = {
+        //         hours: hours,
+        //         minutes: minutes
+        //     }
+        //     players[index].time = time;
+        // })
 
-        site.currnetMap(serverData.map);
-        site.playersAmount(serverData.playersamount);
-        site.maxPlayers(serverData.maxplayers);
-        site.playersList(serverData.palyerslist);
     }
 }
 
@@ -45,27 +29,9 @@ $( document ).ready(function() {
         $('.ttop').tooltip();
     }, 500);
 
-    var pager = new Pager($, ko)
-    window.pager = pager
-    pager.useHTML5history = true
-    pager.Href5.history = History
-    pager.extendWithPage(site)
-    ko.applyBindings(site)
-    pager.startHistoryJs()
-    History.Adapter.bind(window, "statechange", function() {
-        site.currentPage(pager.page.route[0])
-    })
-
-    var url = location.pathname.substr(1)
-    if (url == "statistics" || url == "rules") {
-        site.goto(url);
-    } else {
-        site.goto("statistics");
-    }
-
 
     setTimeout(function() {
-        m_chat.init();
+        // m_chat.init();
     }, 200);
     setTimeout(site.loadAll, 200);
     setInterval(site.loadAll, 5000);
